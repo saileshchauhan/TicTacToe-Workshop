7416eba834de53fc87584e6afc26b83015229747
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-07 11:42:09 +0530

    [UC1]

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TicTacToeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToeGame ticTacToe = new TicTacToeGame();
+            ticTacToe.Create_Board();
+          
+        }
+
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeProject
+{
+    class TicTacToeGame
+    {
+        char[] boardArray = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        public void Create_Board()
+        {
+            Console.WriteLine(" |"+boardArray[1]+"|"+boardArray[2]+"|"+boardArray[3]+"|");
+            Console.WriteLine(" |" + boardArray[4]+"|" + boardArray[5]+ "|" + boardArray[6]+ "|");
+            Console.WriteLine(" |" + boardArray[7]+ "|" + boardArray[8]+"|" + boardArray[9]+"|");
+        }
+    }
+}
